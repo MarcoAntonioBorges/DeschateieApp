7d57aa5bc9acf70a173e2e1146a9b7fde8cda197
Marco Antonio B. Oliveira <marco@oc7086750503.ibm.com>
2019-10-20 20:59:00 -0300

    Commit pom.xml

---
+++ target/classes/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO T_DES_USUARIO(nome, email, data_nascimento, login, senha, numero_permissao, genero) VALUES('Marco Antonio Oliveira', 'marco@email.com', '2000-03-30', 'marco', '123', 'P1', 'MASCULINO');
+INSERT INTO T_DES_USUARIO(nome, email, data_nascimento, login, senha, numero_permissao, genero) VALUES('Larissa Cortela', 'larissa@email.com', '1929-03-30', 'lari', '123', 'P0', 'FEMININO');
+INSERT INTO T_DES_USUARIO(nome, email, data_nascimento, login, senha, numero_permissao, genero) VALUES('Lucas Mendeiro Padeiro', 'lucas@email.com', '1998-03-30', 'lucas', '123', 'P0', 'MASCULINO');
+INSERT INTO T_DES_USUARIO(nome, email, data_nascimento, login, senha, numero_permissao, genero) VALUES('Pedro Antonio Oliveira', 'pedro@email.com', '2010-03-30', 'pedro', '123', 'P0', 'MASCULINO');
+INSERT INTO T_DES_USUARIO(nome, email, data_nascimento, login, senha, numero_permissao, genero) VALUES('Gabriela Jesus', 'gabi@email.com', '2008-03-30', 'gabi', '123', 'P0', 'FEMININO');
+
+INSERT INTO T_DES_USUARIO(nome, email, data_nascimento, login, senha, numero_permissao, genero) VALUES('Enzo Bento Elias Fernandes', 'enzo@email.com', '1966-05-14', 'enzo', '123', 'P0', 'MASCULINO');
+INSERT INTO T_DES_USUARIO(nome, email, data_nascimento, login, senha, numero_permissao, genero) VALUES('Bruna Sophia Silvana Monteiro', 'bruna@email.com', '1969-03-30', 'bruna', '123', 'P0', 'FEMININO');
+INSERT INTO T_DES_USUARIO(nome, email, data_nascimento, login, senha, numero_permissao, genero) VALUES('Priscila Giovana Moreira', 'priscila@email.com', '1955-03-30', 'pri', '123', 'P0', 'FEMININO');
+INSERT INTO T_DES_USUARIO(nome, email, data_nascimento, login, senha, numero_permissao, genero) VALUES('Márcio Bento Moura', 'marcio@email.com', '1987-03-30', 'marcio', '123', 'P0', 'MASCULINO');
+INSERT INTO T_DES_USUARIO(nome, email, data_nascimento, login, senha, numero_permissao, genero) VALUES('Samuel Diogo Rafael Figueiredo', 'samuca@email.com', '1953-03-30', 'samuel', '123', 'P0', 'MASCULINO');
+
+INSERT INTO T_DES_PSICOLOGO(biografia, crp, formacao, telefone, valor_consulta, codigo) VALUES('Jovem de 19 anos psicologo desde da infancia', 111, 'PSICOLOGIA OCIDENTAL', 11993949612, 300, 1);
